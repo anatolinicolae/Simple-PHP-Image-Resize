12597216e503ed85046cba622e6bb41e18c4d6aa
Anatoli Nicolae <desk@anatolinicolae.com>
2012-09-29 16:12:55 +0200

    Fixed get functions from URLs

--- example.php
+++ example.php
@@ -15,10 +15,39 @@ Author URI: http://www.anatolinicolae.com
  * @since 1.0
  */
 
+// Get the file from URL by cURL if installed, else by file_get_contents() function
+if (!function_exists('ts_file_contents')) {
+	function ts_file_contents($url) {
+		if(function_exists('curl_init')) {
+			$ch = curl_init();
+			$timeout = 0; // set to zero for no timeout
+			
+			curl_setopt ($ch, CURLOPT_URL, $url);
+			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	
+			$file_contents = curl_exec($ch);
+	
+			if (!$file_contents) {
+				print_r(curl_getinfo($ch));
+				die;
+			}
+	
+			curl_close($ch);
+		}
+		
+		else {
+			$file_contents = file_get_contents($url);
+		}
+		
+		return $file_contents;
+	}
+}
+
 // Base64 encoding function with compression magic
 if (!function_exists('ts_b64Image')) {
 	function ts_b64Image($url, $size) {
-		$resize = ts_file_contents(ASI_PLUGIN_URL . '/core/resize.php?w=' . $size . '&i=' . $url . '');
+		$resize = ts_file_contents('resize.php?w=' . $size . '&i=' . $url . '');
 		
 		if(ini_get("zlib.output_compression")) {
 		    $b64Image = base64_encode(gzcompress($resize, 9));
@@ -35,6 +64,6 @@ $width = 400; // Size in pixels, integer values only
 
 print '<img src="data:image/jpg;base64,' . ts_b64Image($url, $width) . '" width="' . $width . '" />'; // Outputs the image encoded in base64
 
-print '<img src="resize.php?i=' . $url . '&w=' . $width . '" width="' . $width . '" />'; // Outputs the image form the script PHP
+print '<img src="resize.php?w=' . $width . '&i=' . $url . '" width="' . $width . '" />'; // Outputs the image form the script PHP
 
 ?>
